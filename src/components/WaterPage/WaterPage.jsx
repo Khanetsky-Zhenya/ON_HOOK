d5647f5efc32083af5be925090a57bfa52d50974
Khanetsky-Zhenya <khanetsky.zhenya@mail.ru>
2021-10-26 21:08:04 +0300

    update json data file

--- src/components/WaterPage/WaterPage.jsx
+++ src/components/WaterPage/WaterPage.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+
+import { useParams } from "react-router-dom";
+
 import { ACTIONS } from "../../redux/constants";
 import styles from "./WaterPage.module.css";
-import { useParams } from "react-router-dom";
 
 export const WaterPage = () => {
   const dispatch = useDispatch();
@@ -22,12 +24,24 @@ export const WaterPage = () => {
     <div className={styles.waterPage}>
       {currentWater ? (
         <div className={styles.waterWrapper}>
-          <div>Название: {currentWater.name}</div>
-          <div>Краткое описание: {currentWater.shortDescription}</div>
-          <div>Площадь {currentWater.square}</div>
-          <div> Максимальная глубина {currentWater.depth}</div>
-          <div>Длина {currentWater.length}</div>
-          <div>Краткое описание: {currentWater.fullDescription}</div>
+          <div className={styles.waterWrapper_name}>
+            Название: {currentWater.name}
+          </div>
+          <div className={styles.waterWrapper_shortDescr}>
+            Краткое описание: {currentWater.shortDescription}
+          </div>
+          <div className={styles.waterWrapper_square}>
+            Площадь: {currentWater.square}
+          </div>
+          <div className={styles.waterWrapper_depth}>
+            Максимальная глубина: {currentWater.depth}
+          </div>
+          <div className={styles.waterWrapper_length}>
+            Длина: {currentWater.length}
+          </div>
+          <div className={styles.waterWrapper_fullDescr}>
+            Полное описание: {currentWater.fullDescription}
+          </div>
         </div>
       ) : (
         <div>Идет загрузка водоема</div>
