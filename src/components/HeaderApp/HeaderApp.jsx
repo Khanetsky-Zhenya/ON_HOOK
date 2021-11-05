d1067da1751eaaf8711f6bfbd1c368aa1cdc556b
Khanetsky-Zhenya <khanetsky.zhenya@mail.ru>
2021-11-05 09:57:25 +0300

    fix logo on hook. status - fail

--- src/components/HeaderApp/HeaderApp.jsx
+++ src/components/HeaderApp/HeaderApp.jsx
@@ -4,10 +4,17 @@ import styles from "./HeaderApp.module.css";
 export const HeaderApp = (props) => {
   const { title } = props;
   return (
-    <div className={styles.header}>
-      <h2 className={styles.header__title}>{title}</h2>
-      <div className={styles.header__separator}></div>
-      <p className={styles.header__subTitle}>первый сайт рыбака</p>
+    <div className={styles.header_main}>
+      <div className={styles.header_mainL}>
+      </div>
+      <div className={styles.header}>
+        <h2 className={styles.header__title}>{title}</h2>
+        <div className={styles.header__separator}></div>
+        <div className={styles.header__logo}></div>
+        <p className={styles.header__subTitle}>первый сайт рыбака</p>
+      </div>
+      <div className={styles.header_mainR}>
+      </div>
     </div>
   );
 };
