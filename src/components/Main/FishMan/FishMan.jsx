b280df0be7b75fdec01b821aafc658f33eaa74e4
Khanetsky-Zhenya <khanetsky.zhenya@mail.ru>
2021-11-05 13:53:29 +0300

    add new style for fishlist

--- src/components/Main/FishMan/FishMan.jsx
+++ src/components/Main/FishMan/FishMan.jsx
@@ -9,25 +9,36 @@ export const FishMan = ({ fishmans }) => {
       {fishmans.map((item) => {
         return (
           <div className={styles.Card}>
+            <div className={styles.CardL}>
             <img
               className={styles.Card__avatar}
               src={item.avatar ? images[item.avatar] : images.defaultAvatar}
               alt="your avatar"
             />
-            <div>
+            </div>
+            <div className={styles.CardR}>
               <div className={styles.Card__FullName}>
                 <p className={styles.Card__Name}>{item.name}</p>
                 <p className={styles.Card__SecondName}>{item.secondName}</p>
               </div>
-              <p className={styles.Card__FishType}>
-                Вид ловли: <span>{item.fishType}</span>
-              </p>
-              <p className={styles.Card__Trophy}>
-                Трофей: <span>{item.trophy}</span>
-              </p>
-              <p className={styles.Card__TotalCatch}>
-                Общий улов: <span>{item.totalCatch} килограмм</span>
-              </p>
+              <div className={styles.Card__Achiev}>
+                <div className={styles.Card__Achiev_group1}>
+                  <p className={styles.Card__Achiev_groupall}>Вид ловли:</p>
+                  <p className={styles.Card__Achiev_groupUserdata}>{item.fishType}</p>
+                </div>
+                <div className={styles.Card__Achiev_groupBorder}></div>
+                <div className={styles.Card__Achiev_group2}>
+                  <p className={styles.Card__Achiev_groupall}>Трофей:</p>
+                  <p className={styles.Card__Achiev_groupUserdata}>{item.trophy}</p>
+                </div>
+                <div className={styles.Card__Achiev_groupBorder}></div>
+                <div  className={styles.Card__Achiev_group3}>
+                  <p className={styles.Card__Achiev_groupall}>Общий улов:</p>
+                  <p className={styles.Card__Achiev_groupUserdata}>{item.totalCatch} кг</p>
+                </div>
+
+              </div>
+              
             </div>
           </div>
         );
